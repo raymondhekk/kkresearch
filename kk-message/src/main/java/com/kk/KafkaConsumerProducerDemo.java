1de8a3146b671579f78c2e3a7ba56b7921c88d88
hekun <hekun@zhai.me>
2015-12-18 22:20:03 +0800

    add kafka code

---
+++ kk-message/src/main/java/com/kk/KafkaConsumerProducerDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.kk;
+
+/**
+ * @author kk
+ *
+ */
+public class KafkaConsumerProducerDemo  
+{  
+    public static void main(String[] args)  
+    {  
+        KafkaProducer producerThread = new KafkaProducer(KafkaProperties.topic);  
+        producerThread.start();  
+  
+        KafkaConsumer consumerThread = new KafkaConsumer(KafkaProperties.topic);  
+        consumerThread.start();  
+    }  
+} 
